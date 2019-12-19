38eea45b0662ee5e90a6d0535a59d8ab617aa535
MatobaSeiki <e1b15097@st.oit.ac.jp>
2019-12-19 18:12:14 +0900

    add front

---
+++ visualize/src/components/VisualizeBack.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <span v-for='i of len' v-bind:key='i'>
+        <span class="word" v-bind:style="{ backgroundColor: calcWeight(weight[i-1]) }" v-bind:title="titleMessage(weight[i-1])">
+            {{ sentence[i-1] }}
+        </span>
+    </span>
+  </div>
+</template>
+
+<script>  
+export default {
+  props: {
+    sentence: Array,
+    weight: Array
+  },
+  data() {
+    return {
+        len: this.sentence.length
+    }
+  },
+  methods: {
+    calcWeight (weight) {
+      const red = 255
+      const green = Math.floor(255 * (1 - Number(weight)))
+      const blue = Math.floor(255 * (1 - Number(weight)))
+      return '#' + red.toString(16).toUpperCase().substring(0, 2) +
+             green.toString(16).toUpperCase().substring(0, 2) +
+             blue.toString(16).toUpperCase().substring(0, 2)
+    },
+    titleMessage (weight) {
+      return 'weight：' + weight
+    }
+  }
+}
+</script>
+
+<style scoped>
+.word {
+  cursor: default;
+  font-size: 120%;
+}
+.word:hover {
+  opacity: 0.6;
+}
+</style>
---
+++ visualize/src/components/VisualizeFront.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="visualize">
+    <button class="container" v-on:click="nextId">next</button>
+    <button class="container" v-on:click="backId">back</button>
+    <transition appear>
+      <div v-show="show">
+        <VisualizeBack :sentence="attention[currentId].sentence"
+         :weight="attention[currentId].attention_weight" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import VisualizeBack from './VisualizeBack.vue'
+
+export default {
+  components: {
+    VisualizeBack
+  },
+  data() {
+    return {
+      currentId: 0,
+      attention: require("../assets/features.json"),
+      show: true
+    }
+  },
+  methods: {
+    nextId: function () {
+      if (this.currentId == this.attention.length - 1) {
+        console.log(this.currentId)
+        this.currentId = 0
+      }else{
+        console.log(this.currentId)
+        this.currentId += 1
+      }
+    },
+    backId: function () {
+      if (this.currentId != 0){
+        console.log(this.currentId)
+        this.currentId -= 1
+      }else{
+        console.log(this.currentId)
+        this.currentId = this.attention.length - 1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  font-family: "Yu Gothic", YuGothic, Verdana, 'Hiragino Kaku Gothic ProN','Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', 'メイリオ', Meiryo, sans-serif;
+  text-align: center;
+  cursor: pointer;
+}
+.container:hover{
+  opacity: 0.6;
+}
+</style>
