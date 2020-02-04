c42b48126a369a46a01b4e2872c23e9a95d65007
MatobaSeiki <e1b15097@st.oit.ac.jp>
2020-02-04 15:22:33 +0900

    add list

---
+++ visualize/src/components/VisualizeList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="attentionList">
+    <span v-for="attention in attentionList" v-bind:key="attention">
+      <li v-for="s in attention" v-bind:key="s">
+        {{ s }}
+      </li>
+    </span>
+  </div>
+</template>
+
+<script>
+import VisualizeBack from './VisualizeBack.vue'
+
+export default {
+  components: {
+    //VisualizeBack
+  },
+  data() {
+    return {
+      attentionList: require("../assets/features.json")
+    }
+  }
+}
+</script>
